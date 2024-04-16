6497d6c96e1bb03e3a6da06e4ad50e5e7d04e26e
Qumber Abbas <qumber.abbas@justeattakeaway.com>
2024-04-16 20:47:10 +0200

    Test

---
+++ scripts/script.rb
@@ -0,0 +1,22 @@
+require 'git'
+
+class MyGit
+  def self.git_last_merged_issues(git_path, pattern)
+    g = Git.open(git_path)
+    latest_release_tag = "3.7.0"
+    tasks_list = g.log(200).between(latest_release_tag, "HEAD").count
+    print(tasks_list)
+  end
+
+end
+
+class MyPipeline
+  @release_tag_pattern = "[0-9]*.[0-9]*.[0-9]*"
+
+  def self.release_run(git_path)
+    MyGit.git_last_merged_issues(git_path, @release_tag_pattern)
+  end
+end
+
+
+MyPipeline.release_run("/Users/qumber.abbas/Documents/increaseTags")
