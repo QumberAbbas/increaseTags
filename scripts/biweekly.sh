c49fb18154d50ce12498a1918c7b9d8d8131629e
Qumber Abbas <qumber.abbas@justeattakeaway.com>
2024-04-24 18:41:20 +0200

    TT

--- scripts/biweekly.sh
+++ scripts/biweekly.sh
@@ -2,5 +2,10 @@ current_date=$(date +%Y-%m-%d)
 start=$(date -d $1 +%s)
 end=$(date -d $current_date +%s)
 weekdiff=$(((end-start) / 60 / 60 / 24 / 7))
+
+echo "Start date: $start"
+echo "End date: $end"
+echo "Week difference: $weekdiff"
+
 weekindex=$((weekdiff % 2))
 echo $weekindex
